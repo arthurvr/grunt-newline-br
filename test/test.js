cadc673f00c0f8f2114d20d0c370f8fb3511762d
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 22:25:42 +0200

    ES2015ify tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var test = require('ava');
+import fs from 'fs';
+import path from 'path';
+import test from 'ava';
 
-test('Should prepend newlines with a `<br>`', function (t) {
-	var result = fs.readFileSync(path.join(__dirname, 'tmp/fixture.txt'));
+test('Should prepend newlines with a `<br>`', t => {
+	const result = fs.readFileSync(path.join(__dirname, 'tmp/fixture.txt'));
+
 	t.is(result.toString(), 'foo<br>\nbar<br>\n');
 	t.end();
 });
